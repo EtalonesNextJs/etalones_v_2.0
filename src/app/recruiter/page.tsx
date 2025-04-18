597d0220e557bdb36c3952475e2a75ae699f98db
Your Name <your_email@example.com>
2025-04-18 15:38:52 +0300

    plus recruiter

---
+++ src/app/recruiter/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    
+         
+          <><h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
+          Page title
+      </h2><p className="mt-4 text-lg text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+              cupiditate, omnis officia deleniti eos, id itaque quidem, quis
+              aspernatur velit illum voluptas, veritatis quisquam atque asperiores
+              tempora.
+          </p></>
+
+        )}
